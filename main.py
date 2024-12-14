4b275d1c39c454f9aeec5d36aeeee65dd2df36d1
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-14 13:46:03 +0300

    Define server and create host,port

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import socket
 
-socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT =8080
+
+sever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sever_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sever_socket.bind((SERVER_HOST,SERVER_PORT))
+
+sever_socket.listen(5)
+
+print(f"Listerning on port {SERVER_PORT} ...")
