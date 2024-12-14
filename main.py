d17689732be92dd8b070c9af693af75a5eb7b53d
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-14 13:52:16 +0300

    Define server.accept for client socket and client address

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import socket
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORT =8080
+SERVER_PORT =8095
 
-sever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sever_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sever_socket.bind((SERVER_HOST,SERVER_PORT))
+server_socket.bind((SERVER_HOST,SERVER_PORT))
 
-sever_socket.listen(5)
+server_socket.listen(5)
 
-print(f"Listerning on port {SERVER_PORT} ...")
+print(f"Listening on port {SERVER_PORT} ...")
+
+client_socket, client_address = server_socket.accept()
+print(client_socket)
+print(client_address)
