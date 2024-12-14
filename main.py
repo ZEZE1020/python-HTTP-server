20701db8dc2759849176299b90332ecc68a7e659
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-14 16:08:45 +0300

    Add static files

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import socket
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORT =8095
+SERVER_PORT = 8095
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server_socket.bind((SERVER_HOST,SERVER_PORT))
 
-server_socket.listen(5)
+server_socket.listen(1)
 
 print(f"Listening on port {SERVER_PORT} ...")
 
-client_socket, client_address = server_socket.accept()
-print(client_socket)
-print(client_address)
+while True:
+        client_socket, client_address = server_socket.accept()
+        request = client_socket.recv(150000).decode()
+        print(request)
+        headers = request.split('\n')
+        print(headers[0])
+        first_header_components = headers[0].split()
+
+        http_method = first_header_components[0]
+        path = first_header_components[1]
+
+        
+        if http_method == 'GET':
+            if path == '/':
+                fin = open('index.html')
+            elif path =='/servers':
+                fin = open('servers.html')
+      
+            content = fin.read()
+            fin.close()
+            response  = 'HTTP/1.1 200 OK\n\n'+ content
+            
+        else:
+            response = 'HTTP/1.1 405 Method Not Allowed\n\nAllow: GET,'
+
+        client_socket.sendall(response.encode())
+        client_socket.close()
